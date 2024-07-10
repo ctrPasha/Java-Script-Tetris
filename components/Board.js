dc6af5b0e910bc46812a787f6eda06c0ba98947f
ctrPasha <pashes12@gmail.com>
2024-07-10 19:23:16 +0200

    Tetris Collab w/ devin

---
+++ components/Board.js
@@ -0,0 +1,39 @@
+class Board {
+  constructor(context) {
+    this.context = context;
+    this.grid = this.getEmptyBoard();
+  }
+  /* Array.from Creates an array with Rows number of elements(in this case 20 cells)
+    The callback creates an array for the columns which then returns a 10x20 grid.
+    The array is filled with 0's. In short all this is doing is creating a 2D array/grid
+    with the rows and columns cells initially set to 0.                                             
+  */
+ 
+  getEmptyBoard() {
+    return Array.from(
+      {length: ROWS}, () => Array(COLS).fill(0)
+    );
+  }
+};
+
+const drawGrid = () => {
+  context.strokeStyle = "white";
+  context.lineWidth = 0.05;
+
+  // Draws Vertical Lines
+  for (let i = 0; i <= COLS; i++) {
+    context.beginPath();
+    context.moveTo(i, 0);
+    context.lineTo(i, ROWS);
+    context.stroke();
+  }
+  
+  // Draws horizontal lines
+  for (let j = 0; j <= ROWS; j++) {
+    context.beginPath();
+    context.moveTo(0, j);
+    context.lineTo(COLS, j);
+    context.stroke();
+  }
+
+}
---
+++ components/index.js
@@ -0,0 +1,26 @@
+const canvas = document.getElementById("board");
+const context = board.getContext('2d');
+
+const COLS = 10;
+const ROWS = 20;
+const CELL_SIZE = 20;
+
+// Setting canvas dimensions
+context.canvas.width = COLS * CELL_SIZE;
+context.canvas.height = ROWS * CELL_SIZE;  
+
+// scaling the blocks
+context.scale(CELL_SIZE, CELL_SIZE);
+
+function play() {
+  let playBtn = document.getElementById("play-btn");
+  board = new Board(context);
+  console.table(board.grid);
+  // On click, the button will disappear 
+  playBtn.style.display = 'none';
+
+  // Renders the cells/grid onto the screen when the start button is pressed
+  drawGrid();
+}
+
+
